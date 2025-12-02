3f2b3beb05135eb05d3ca08335810294469fe988
mtlaguerre <245204912+mtlaguerre@users.noreply.github.com>
2025-12-02 10:46:00 -0500

    basic functionality complete

--- src/main/java/com/mtlaguerre/inventory_management/services/ProductService.java
+++ src/main/java/com/mtlaguerre/inventory_management/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.mtlaguerre.inventory_management.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,16 @@ public class ProductService {
 
     public List<Product> findAllProducts() {
         return productRepository.findAll();
-    } 
+    }
+
+    public List<Product> findProductByName(String name) throws IllegalArgumentException {
+        Optional<List<Product>> products =  productRepository.findByName(name);
+
+        if (products.isPresent()) {
+            return products.get();      // returns object from the optional
+        }
+        else {
+            throw new IllegalArgumentException("No product with that name.");
+        }
+    }
 }
