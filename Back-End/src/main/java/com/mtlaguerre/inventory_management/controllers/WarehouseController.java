0b847bdeb544247f675670f864b72e511bdb838a
mtlaguerre <245204912+mtlaguerre@users.noreply.github.com>
2025-12-11 22:56:52 -0500

    added findAllWarehouses
    added @RequestMapping to WarehouseController
    added JpaRepository to WarehouseRepository
    connected controller to service
    connected service to repository

--- Back-End/src/main/java/com/mtlaguerre/inventory_management/controllers/WarehouseController.java
+++ Back-End/src/main/java/com/mtlaguerre/inventory_management/controllers/WarehouseController.java
@@ -1,8 +1,39 @@
 package com.mtlaguerre.inventory_management.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mtlaguerre.inventory_management.models.Warehouse;
+import com.mtlaguerre.inventory_management.services.WarehouseService;
+
+
 @RestController
+@RequestMapping("/warehouses")
 public class WarehouseController {
+
+    private final WarehouseService warehouseService;
+
+    // constructor injection
+    public WarehouseController(WarehouseService warehouseService) {
+        this.warehouseService = warehouseService;
+    }
+
+    // GET /warehouses
+    @GetMapping
+    public ResponseEntity<List<Warehouse>> findAllWarehouses() {
+        
+        try {
+            List<Warehouse> warehouses = warehouseService.findAllWarehouses();
+            return new ResponseEntity<>(warehouses, HttpStatus.OK);                                 // return 200 if successful
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().header("message", e.getMessage()).build();  // otherwise return 500
+        }
+    }
+    
 
 }
--- Back-End/src/main/java/com/mtlaguerre/inventory_management/services/WarehouseService.java
+++ Back-End/src/main/java/com/mtlaguerre/inventory_management/services/WarehouseService.java
@@ -1,8 +1,23 @@
 package com.mtlaguerre.inventory_management.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.mtlaguerre.inventory_management.models.Warehouse;
+import com.mtlaguerre.inventory_management.repositories.WarehouseRepository;
 
 @Service
 public class WarehouseService {
 
+    private final WarehouseRepository warehouseRepository;
+
+    // constructor injection
+    public WarehouseService(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    public List<Warehouse> findAllWarehouses() {
+        return warehouseRepository.findAll();
+    }
 }
